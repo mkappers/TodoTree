d6c77fcddbde5c0286fe2cedbc727d6f196fc51b
mkappers <maurits.kappers@gmail.com>
2018-05-30 17:31:32 +0200

    Extended tree and treetest functionality, ready for preliminary tree/node mashup

--- ttcore/tree.py
+++ ttcore/tree.py
@@ -2,16 +2,100 @@
 # https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
 
 class TreeNode():
-    def __init__(self, parent = None, children = []):
+    def __init__(self, parent = None, children = None):
+        """Initialize a tree node.
+
+        If no children are passed, children will be initialized as an empty list.
+
+        Keyword arguments:
+        parent -- the parent of this node (default None)
+        children -- the children of this node (default None)
+        """
         self.parent = parent
         self.children = children
 
+        if self.children is None:
+            self.children = []
 
     # TODO Magic methods (eq etc), add children, size, add branch, pruning, and more.
+    def appendChild(self, child):
+        """Append a child to this node.
+
+        Keyword arguments:
+        child -- the child to append
+        """
+        child.parent = self
+        self.children.append(child)
+
+    def insertChild(self, index, child):
+        """Insert a child at this specific index of the children collection.
+
+        Keyword arguments:
+        index -- the index where the child is inserted
+        child -- the child to insert
+        """
+        child.parent = self
+        self.children.insert(index, child)
+
+    def insertChildBefore(self, a, b):
+        """Insert (child) a before (child) b in this node.
+
+        If b does not exist, a ValueError is raised.
+
+        Keyword arguments:
+        a -- the child to be inserted
+        b -- the child after which a will be inserted
+        """
+        try:
+            index = self.children.index(b)
+        except ValueError:
+            raise
+
+        self.insertChild(index, a)
+
+    def removeChild(self, child):
+        """Remove child from this node.
+
+        Returns child.
+
+        Keyword arguments:
+        child -- the child to be removed
+        """
+        child.parent = None
+        self.children.remove(child)
+        return child
+
+    def cutTree(self):
+        """Cut this tree.
+
+        Returns this node.
+        """
+        if self.parent is not None:
+            self.parent.removeChild(self)
+        return self
+
+    def cut(self):
+        """Cut only this node.
+
+        Childrens' parent is set to this node's parent.
+        """
+        if self.parent is not None:
+            for child in self.children:
+                self.parent.insertChildBefore(child, self)
+        else:
+            for child in self.children:
+                child.parent = None
+
+        self.children = []
+        return self.parent.removeChild(self)
+
 
     # Traversals:
-
     # Depth first
+    # def getDescendantsPreOrder(self):
+    #     _, *tail = self.getPreOrder()
+    #     return tail
+
     def getPreOrder(self):
         preOrderList = [self]
 
@@ -23,10 +107,18 @@ class TreeNode():
 
         return preOrderList
 
-    def getPreOrderDescendants(self):
-        _, *tail = self.getPreOrder()
-        return tail
-
     # Level Order - Top to bottom
     def getLevelOrder(self):
-        pass
+        levelOrderList = [self]
+        levelChildren = self.children
+
+        while not len(levelChildren) == 0:
+            levelOrderList.extend(levelChildren)
+            levelChildrenTemp = []
+
+            for child in levelChildren:
+                levelChildrenTemp.extend(child.children)
+
+            levelChildren = levelChildrenTemp
+
+        return levelOrderList
--- tttest/treetest.py
+++ tttest/treetest.py
@@ -7,3 +7,164 @@ class TestTree(unittest.TestCase):
 
         self.assertTrue(tree.parent == None)
         self.assertTrue(len(tree.children) == 0)
+        self.assertTrue(tree.children == [])
+
+    def test_add_children(self):
+        tree = TreeNode()
+
+        tree.appendChild(TreeNode())
+        tree.appendChild(TreeNode())
+        tree.appendChild(TreeNode())
+
+        self.assertTrue(len(tree.children) == 3)
+
+    def test_remove_child(self):
+        tree = TreeNode()
+
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+
+        tree.appendChild(a)
+        tree.appendChild(b)
+        tree.appendChild(c)
+        tree.appendChild(d)
+
+        self.assertTrue(tree.children == [a,b,c,d])
+
+        tree.removeChild(a)
+        self.assertTrue(tree.children == [b,c,d])
+
+        tree.removeChild(c)
+        self.assertTrue(tree.children == [b,d])
+
+        tree.removeChild(d)
+        self.assertTrue(tree.children == [b])
+
+    def test_insert_child(self):
+        tree = TreeNode()
+
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+        e = TreeNode()
+
+        tree.insertChild(0,a)
+        self.assertTrue(tree.children == [a])
+
+        tree.insertChild(0,c)
+        self.assertTrue(tree.children == [c,a])
+
+        tree.insertChild(1,b)
+        self.assertTrue(tree.children == [c,b,a])
+
+        # Weird inserts, but work due to python list implementation
+        tree.insertChild(5,d)
+        self.assertTrue(tree.children == [c,b,a,d])
+        tree.insertChild(-1, e)
+        self.assertTrue(tree.children == [c,b,a,e,d])
+
+    def test_insert_child_before(self):
+        tree = TreeNode()
+
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+
+        tree.appendChild(a)
+        tree.insertChildBefore(b,a)
+        self.assertTrue(tree.children == [b,a])
+
+        tree.insertChildBefore(c,a)
+        self.assertTrue(tree.children == [b,c,a])
+
+        with self.assertRaises(ValueError):
+            tree.insertChildBefore(d, TreeNode())
+
+    def test_cut_tree(self):
+        tree = TreeNode()
+
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+
+        tree.appendChild(a)
+        tree.appendChild(b)
+        b.appendChild(c)
+        b.appendChild(d)
+
+        self.assertTrue(tree.getLevelOrder() == [tree,a,b,c,d])
+
+        b2 = b.cutTree()
+
+        self.assertTrue(tree.getLevelOrder() == [tree,a])
+        self.assertTrue(b2.parent == None)
+        self.assertTrue(b2.getLevelOrder() == [b,c,d])
+
+        self.assertTrue(tree.cutTree() == tree)
+
+        # Also test tree insert
+        tree.appendChild(b2)
+        self.assertTrue(tree.getLevelOrder() == [tree,a,b,c,d])
+
+    def test_cut_node(self):
+        tree = TreeNode()
+
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+
+        tree.appendChild(a)
+        tree.appendChild(b)
+        b.appendChild(c)
+        b.appendChild(d)
+
+        self.assertTrue(tree.getLevelOrder() == [tree,a,b,c,d])
+
+        b2 = b.cut()
+        self.assertTrue(tree.children == [a,c,d])
+        self.assertTrue(b2.parent == None)
+        self.assertTrue(b2.children == [])
+
+    def test_traversal_preorder(self):
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+        e = TreeNode()
+
+        a.appendChild(b)
+        a.appendChild(c)
+        b.appendChild(d)
+        b.appendChild(e)
+
+        preorderlist_check = [a,b,d,e,c]
+        preorderlist = a.getPreOrder()
+
+        self.assertTrue(preorderlist_check == preorderlist)
+
+    def test_traversal_levelorder(self):
+        a = TreeNode()
+        b = TreeNode()
+        c = TreeNode()
+        d = TreeNode()
+        e = TreeNode()
+
+        a.appendChild(b)
+        a.appendChild(c)
+        b.appendChild(d)
+        b.appendChild(e)
+
+        levelorderlist_check = [a,b,c,d,e]
+        levelorderlist = a.getLevelOrder()
+
+        self.assertTrue(levelorderlist_check == levelorderlist)
+
+
+if __name__ == '__main__':
+    unittest.main()
